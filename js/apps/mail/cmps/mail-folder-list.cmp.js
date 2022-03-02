5fd96adbf253a6107a35e4312a15319edae61573
Eyal Yosipovich <eyalyosi@gmail.com>
2022-03-02 17:07:09 +0200

    selected mail, mail details

--- js/apps/mail/cmps/mail-folder-list.cmp.js
+++ js/apps/mail/cmps/mail-folder-list.cmp.js
@@ -1,13 +1,11 @@
 export default {
     template: `
        <section class="mail-folder-list">
-           <div>Compose</div>
-                <ul>
-                    <li>Inbox</li>
-                    <li>Starred</li>
-                    <li>Sent Mail</li>
-                    <li>Drafts</li>
-                </ul>
+           <div><img src="/png/005-plus.png">Compose</div>
+           <div><img src="/png/002-download.png"> Inbox</div>
+           <div><img src="/png/003-star.png"> Starred</div>
+           <div><img src="/png/004-mail.png"> Sent Mail</div>
+           <div><img src="/png/001-drafts.png"> Drafts</div>
        </section>
     `,
     components: {
--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -5,6 +5,10 @@ export default {
             <div class="logo">
                 <h3>Appsus</h3>
             </div>
+
+            <div class="filter-container">
+
+            </div>
             <nav class="nav-bar">
                 <router-link to="/">Home</router-link> |
                 <router-link to="/note">Note</router-link> |
